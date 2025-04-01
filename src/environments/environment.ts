d7a60f20e455c6ea7e45f8c961e2112b6df8d997
Dawid Maliszewski <dawid@thedavesky.com>
2025-04-01 17:44:12 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     API_URL: 'http://127.0.0.1:8000/api',
-    MEDIA_URL: 'http://your-media-server.com/media/',
-    AUDIO_URL: 'http://your-audio-server.com/audio/'
+    MEDIA_URL: 'https://no-cap-cooking.s3.pl-waw.scw.cloud/images/',
+    AUDIO_URL: 'https://no-cap-cooking.s3.pl-waw.scw.cloud/audio/'
   };
