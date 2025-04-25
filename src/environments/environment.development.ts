b6c55682bed3d1f19046eea54bc8e404fe99a90f
Dawid Maliszewski <dawid@thedavesky.com>
2025-04-25 10:22:03 +0000

    Update API_URL in environment.development.ts to use localhost

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    API_URL: 'http://127.0.0.1:8000/api',
+    API_URL: 'http://localhost:8000/api',
     MEDIA_URL: 'https://nocapcooking-cdn.s3.pl-waw.scw.cloud/images',
     AUDIO_URL: 'https://nocapcooking-cdn.s3.pl-waw.scw.cloud/audio'
 };
