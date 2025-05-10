93b3b30e5799aef0945f821acbac08b8b31c7f07
AshennOne <Dawidrychlik2004@gmail.com>
2025-05-10 14:19:30 +0200

    Adding disclaimer

---
+++ src/app/modules/disclaimer-dialog/disclaimer-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-disclaimer-dialog',
+  imports: [MatDialogModule],
+  templateUrl: './disclaimer-dialog.component.html',
+  styleUrl: './disclaimer-dialog.component.css'
+})
+export class DisclaimerDialogComponent {
+
+}
--- src/app/modules/home-page/home-page.component.ts
+++ src/app/modules/home-page/home-page.component.ts
@@ -7,16 +7,20 @@
  * @property {Router} router - Serwis do nawigacji.
  * @property {signal} welcomeMessage - Wiadomość powitalna.
  */
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { DisclaimerDialogComponent } from '../disclaimer-dialog/disclaimer-dialog.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+
+  readonly dialog = inject(MatDialog);
 
   constructor(private router: Router) {
 
@@ -27,5 +31,11 @@ export class HomePageComponent {
     this.router.navigate(['/browse']);
   }
   welcomeMessage = signal('Idź do strony z przepisami');
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DisclaimerDialogComponent);
+
+    dialogRef.afterClosed().subscribe()
+  }
 
 }
